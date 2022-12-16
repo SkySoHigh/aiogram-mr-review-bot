5b82565fe4750b8c93dc852732bcb4d1dad2a853
SkySoHigh <skysohigh39@gmail.com>
2022-12-16 12:02:53 +0300

    Added task model

---
+++ db/models/tasks.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import enum
+
+from sqlalchemy import Column, Integer, String, Sequence, DateTime, func, Enum
+
+from db.models.base import Base
+
+
+class TaskStates(enum.Enum):
+    NEW = 0
+    ON_REVIEW = 1
+    COMPLETED = 2
+
+
+class TasksModel(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, Sequence('id_seq'), primary_key=True)
+    url = Column(String, nullable=False, unique=True)
+    status = Column(Enum(TaskStates), nullable=False, default=TaskStates.NEW)
+    origin_chat = Column(Integer, nullable=False)
+    applicant = Column(Integer, nullable=False)
+    application_time = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    executor = Column(Integer, nullable=True)
+    acceptance_time = Column(DateTime(timezone=True), nullable=True)
