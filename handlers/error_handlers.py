4c64e4302d1391e5f42cbf3ade3870c0d3e7a8c5
SkySoHigh <skysohigh39@gmail.com>
2022-12-20 23:55:49 +0300

    Added new error msg handler with msg removal

--- handlers/error_handlers.py
+++ handlers/error_handlers.py
@@ -1,6 +1,11 @@
-from aiogram import types
-from aiogram.utils.exceptions import TelegramAPIError, CantInitiateConversation, MessageToForwardNotFound, MessageToEditNotFound
+import asyncio
+from typing import Union
 
+from aiogram import types
+from aiogram.utils.exceptions import TelegramAPIError, CantInitiateConversation, MessageToForwardNotFound, \
+    MessageToEditNotFound
+
+from loader import dp
 from locales import Locale
 
 
@@ -18,3 +23,15 @@ async def errors_handler(update: types.Update, exception: TelegramAPIError):
         await update.callback_query.message.reply(f'{Locale.Error.MESSAGE_TO_EDIT_NOT_FOUND}\n'
                                                   f'{Locale.Error.CONTACT_ADMINISTRATOR}', disable_notification=True)
         return True
+
+
+async def show_error_msg_for_n_seconds(obj: Union[types.Message, types.CallbackQuery], error_msg: str,
+                                       duration: int = 3):
+    msg_for_removal = await obj.bot.send_message(text=f'{error_msg}\n'
+                                                      f'{Locale.Error.ERROR_MSG_WILL_BE_REMOVED_IN}: {duration}s',
+                                                 chat_id=obj.message.chat.id,
+                                                 disable_notification=True,
+                                                 disable_web_page_preview=True,
+                                                 )
+    await asyncio.sleep(duration)
+    await obj.bot.delete_message(chat_id=msg_for_removal.chat.id, message_id=msg_for_removal.message_id)
