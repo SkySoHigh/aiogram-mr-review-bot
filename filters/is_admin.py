bb12f5ec7628019ec14edb07feb7e43f153217d6
SkySoHigh <skysohigh39@gmail.com>
2022-12-20 23:56:42 +0300

    Admin filter refactoring, added async check method

--- filters/is_admin.py
+++ filters/is_admin.py
@@ -1,19 +1,50 @@
+from dataclasses import dataclass
 from typing import Union, List
 
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
-from common import app
+from aiogram.types import ChatType
+
+from loader import app
 
 
+# TODO: There is a bug, when filter check is called for every usage in cb. Gonna use method instead.
+
+@dataclass
 class AdminFilter(BoundFilter):
-
     key = "is_admin"
-
-    def __init__(self, is_admin: bool):
-        self.is_admin = is_admin
+    is_admin: bool
 
     async def check(self, obj: Union[types.Message, types.CallbackQuery]):
         user = obj.from_user
-        if user.id in app.config.common.admins:
+        chat_admins = []
+
+        # If chat is private we will not be able to get admins
+        if obj.message.chat.type != ChatType.PRIVATE:
+            chat_admins.extend([a.user.id for a in await obj.bot.get_chat_administrators(chat_id=obj.message.chat.id)])
+        else:
+            chat_admins.append(obj.from_user.id)
+        print('AAAA')
+        if user.id in [app.config.common.admins, *chat_admins]:
             return self.is_admin is True
-        return self.is_admin is False
+        else:
+            # await self.handle_no_rights_error(obj)
+            return self.is_admin is False
+
+
+async def check(obj: Union[types.Message, types.CallbackQuery], *, additional_ids: List[int] = None) -> bool:
+    user = obj.from_user
+    additional_ids = additional_ids if additional_ids else []
+    chat_admins = []
+
+    chat_admins.extend(additional_ids)
+
+    # If chat is private we will not be able to get admins
+    if obj.message.chat.type != ChatType.PRIVATE:
+        chat_admins.extend([a.user.id for a in await obj.bot.get_chat_administrators(chat_id=obj.message.chat.id)])
+    else:
+        chat_admins.append(obj.from_user.id)
+    if user.id in [app.config.common.admins, *chat_admins]:
+        return True
+    else:
+        return False
